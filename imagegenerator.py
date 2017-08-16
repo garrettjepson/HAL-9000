172ec494b220d632ed45262fc497b726b8f36206
Garrett Jepson <jepsong@oregonstate.edu>
2017-08-16 13:36:58 -0700

    Update imagegenerator.py

--- imagegenerator.py
+++ imagegenerator.py
@@ -3,9 +3,9 @@ from __future__ import print_function
 import numpy as np
 from PIL import Image
 from random import randint
-#import pickle
 
-for n in range(0,4000):
+
+for n in range(0,400):
 	#import time
 	#date_string = time.strftime("%Y-%m-%d-%H:%M:%S")
 #Initialize the matrix- Size (100,100)
@@ -15,29 +15,21 @@ for n in range(0,4000):
 #Initialize the Gaussian Properties
 
 	x0 = randint(1,100); y0 = randint(1,100); sigmax = randint(1,10); sigmay = randint(1,10)
-	#xcenter = (x0)
-	#ycenter = (y0)
-	centers = (x0,y0)
+
+	center = (x0,y0)
+	print (center)
 #Create the Gaussian Function
 
 	def Gaussian(x,y):
 		result = int(round( 255*np.exp(-(x - x0)**2 / (2 * sigmax**2)) * np.exp( -(y - y0)**2 / (2 *sigmay**2))))
 		return result
 
-	#def centers():
-		centers = (x0,y0)
-		#return centers
-		#print(centers)
-
 	for i in range(size):
 		for j in range(size):
 			arr[i][j] = Gaussian(i,j)
-
 
 	im = Image.fromarray(arr)
 	if im.mode !='RGB':
 		im = im.convert('RGB')
 		#im.show()
-		im.save("/home/garrett/train/"+str(centers)+".jpeg", "JPEG")
-
-	print (centers)
+		im.save("/home/garrett/train/"+str(n)+".jpeg", "JPEG")
